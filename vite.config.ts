afd498668d41bd4f32b7ce5528e9cd034694b347
Dominique Loiseau <5888729+domi7777@users.noreply.github.com>
2024-05-28 21:55:01 +0200

    feat(pwa): cache google sheet so that it's available offline

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,31 @@ import {VitePWA} from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate', devOptions: {
-      enabled: true
-    }
-  })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,ts,tsx,css,html,ico,png,svg}'],
+        runtimeCaching: [
+          {
+            // cache google spreadsheet
+            urlPattern: ({url}) => url.origin === 'https://docs.google.com' && url.pathname.startsWith('/spreadsheets/'),
+            handler: 'NetworkFirst', // Use NetworkFirst strategy
+            options: {
+              cacheName: 'api-cache',
+              networkTimeoutSeconds: 10,
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24, // 1 day
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      }
+    })
+  ],
 })
