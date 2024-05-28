799900941af215cb3b76b6230e7ac674b10532c3
Dominique Loiseau <5888729+domi7777@users.noreply.github.com>
2024-05-28 15:47:09 +0200

    feat(pwa): add vite pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate', devOptions: {
+      enabled: true
+    }
+  })],
 })
